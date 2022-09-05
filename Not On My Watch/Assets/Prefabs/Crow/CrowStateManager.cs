d6f5d0bd4699007734e3f4f47ac7a461bcf78b10
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2022-09-05 15:14:43 -0600

    SetUp Crow State Machine

---
+++ Not On My Watch/Assets/Prefabs/Crow/CrowStateManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowStateManager : MonoBehaviour
+{
+    CrowBaseState currentState;
+    public CrowFleeingState FleeingState = new CrowFleeingState();
+    public CrowEatingState EatingState = new CrowEatingState();
+    public CrowTravelState TravelState = new CrowTravelState();
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = TravelState;
+        currentState.EnterState(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.UpdateState(this);
+    }
+
+    public void SwitchState(CrowBaseState state){
+        currentState = state;
+        state.EnterState(this);
+    }
+}
---
+++ Not On My Watch/Assets/Prefabs/Crow/CrowTravelState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CrowTravelState : CrowBaseState
+{
+    public override void EnterState(CrowStateManager crow){
+        Debug.Log("Hello :)");
+    }
+    public override void UpdateState(CrowStateManager crow){
+        //Travel to a crop
+        
+        //find crop
+        //move to crop
+        Debug.Log("Finding crop");
+    }
+    public override void OnCollisionEnter(CrowStateManager crow){
+        
+    }
+}
