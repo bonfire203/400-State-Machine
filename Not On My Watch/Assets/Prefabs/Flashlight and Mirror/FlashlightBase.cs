e337bb207dce1493755f7b4c1c544d4137db9b76
JessnaR <jessna.debsword@gmail.com>
2022-09-19 21:59:22 -0600

    Mirror and Flashlight Update
    Should be fully working. collider and light turn on during correct part of the day/night

--- Not On My Watch/Assets/Prefabs/Flashlight and Mirror/FlashlightBase.cs
+++ Not On My Watch/Assets/Prefabs/Flashlight and Mirror/FlashlightBase.cs
@@ -16,44 +16,45 @@ public class FlashlightBase : MonoBehaviour
     }
     void Update()
     {
-        //if the object is picked up, assume it's on
+        //need some way to test if the object was picked up
         //after 10 seconds, turn it off for a bit, even if picked back up
-        if (timerRunning)
-        {
-            if(timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                Debug.Log("Time to Cooldown");
-                //Turn off collider and light
-                transform.GetChild(0).GetComponent<Collider>().enabled = false;
-                transform.GetChild(1).GetComponent<Light>().enabled = false;
-                timeRemaining = 0;
-                timerRunning = false;
-                cooldownRemaining = 5;
-                cooldownRunning = true;
-            }
-        }
-        if(cooldownRunning)
-        {
-            if(cooldownRemaining > 0)
-            {
-                cooldownRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                Debug.Log("Ready to pick back up again");
-                //Turn on collider and light
-                transform.GetChild(0).GetComponent<Collider>().enabled = true;
-                transform.GetChild(1).GetComponent<Light>().enabled = true;
-                cooldownRemaining = 0;
-                cooldownRunning = false;
-                timeRemaining = 10;
-                timerRunning = true;
-            }
-        }
+        // *** COOLDOWN IF WE WANT TO USE ***
+        // if (timerRunning)
+        // {
+        //     if(timeRemaining > 0)
+        //     {
+        //         timeRemaining -= Time.deltaTime;
+        //     }
+        //     else
+        //     {
+        //         Debug.Log("Time to Cooldown");
+        //         //Turn off collider and light
+        //         transform.GetChild(0).GetComponent<Collider>().enabled = false;
+        //         transform.GetChild(1).GetComponent<Light>().enabled = false;
+        //         timeRemaining = 0;
+        //         timerRunning = false;
+        //         cooldownRemaining = 5;
+        //         cooldownRunning = true;
+        //     }
+        // }
+        // if(cooldownRunning)
+        // {
+        //     if(cooldownRemaining > 0)
+        //     {
+        //         cooldownRemaining -= Time.deltaTime;
+        //     }
+        //     else
+        //     {
+        //         Debug.Log("Ready to pick back up again");
+        //         //Turn on collider and light
+        //         transform.GetChild(0).GetComponent<Collider>().enabled = true;
+        //         transform.GetChild(1).GetComponent<Light>().enabled = true;
+        //         cooldownRemaining = 0;
+        //         cooldownRunning = false;
+        //         timeRemaining = 10;
+        //         timerRunning = true;
+        //     }
+        // }
     }
 
     private void OnTriggerEnter(Collider other) 
