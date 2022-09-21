db2f5be7a7e5867470369aa35d2d9b855547823e
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2022-09-21 08:59:04 -0600

    Crops spawn in Circle

--- Not On My Watch/Assets/Scripts/SpawnCropManager.cs
+++ Not On My Watch/Assets/Scripts/SpawnCropManager.cs
@@ -5,14 +5,53 @@ using UnityEngine;
 public class SpawnCropManager : MonoBehaviour
 {
     //public GameObject TargetObject;
-
-    public GameObject cropObj;
+    public int spawnLimit = 0;
+    public GameObject prefab;
+    private Vector3 playerVec = new Vector3(0f,0f,0f);
     // Start is called before the first frame update
     void Awake()
     {
-        for(int i = 0; i < 10; i ++){
-            Vector3 spawnPos = new Vector3(Random.Range(20, 0), 3, Random.Range(20, 0));
-            Instantiate(cropObj, spawnPos, Quaternion.identity);
+        //basically just checking if this is the first instance or not.
+        spawnLimit = MainManager.Instance.cornStart;
+        if(spawnLimit < 1){
+            spawnLimit = 5;
+        }
+
+        float angle = DetermineAngle();
+        float radius = DetermineRadius();
+        Vector3 center = playerVec;
+        
+        for(int row = 0; row < 50; row ++){
+            Vector3 pos = RandomCircle(center, radius, angle);
+            Instantiate(prefab, pos, Quaternion.identity);
+        
         }
     }
+
+    private float DetermineAngle(){
+        int num = MainManager.Instance.cornStart;
+        if(num > 500){
+            return 360;
+        }else if(num <= 500 && num > 250){
+            return 270;
+        }else if(num <= 250 && num > 100){
+            return 180;
+        }else{
+            return 90;
+        }
+    }
+
+    private float DetermineRadius(){
+        return 5;
+    }
+
+    Vector3 RandomCircle (Vector3 center, float radius, float angle){
+         float ang = Random.value * angle;
+         float nRadius = (Random.value * radius) + 5;
+         Vector3 pos;
+         pos.x = center.x + nRadius * Mathf.Sin(ang * Mathf.Deg2Rad);
+         pos.y = center.y;
+         pos.z = center.z + + nRadius * Mathf.Cos(ang * Mathf.Deg2Rad);
+         return pos;
+     }
 }
