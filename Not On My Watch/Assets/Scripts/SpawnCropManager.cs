aee3716f95aa0be142fedc9d2c60ec7bac277d92
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2022-09-26 20:03:55 -0600

    my unity crashed :)

--- Not On My Watch/Assets/Scripts/SpawnCropManager.cs
+++ Not On My Watch/Assets/Scripts/SpawnCropManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class SpawnCropManager : MonoBehaviour
 {
     //public GameObject TargetObject;
-    public int spawnLimit = 0;
+    public int spawnLimit;
     public GameObject prefab;
+
     private Vector3 playerVec = new Vector3(0f,0f,0f);
     // Start is called before the first frame update
     void Awake()
     {
         //basically just checking if this is the first instance or not.
-        spawnLimit = MainManager.Instance.cornStart;
+        //spawnLimit = MainManager.Instance.cornStart;
         if(spawnLimit < 1){
             spawnLimit = 5;
         }
@@ -21,16 +22,20 @@ public class SpawnCropManager : MonoBehaviour
         float radius = DetermineRadius();
         Vector3 center = playerVec;
         
-        for(int row = 0; row < 50; row ++){
-            Vector3 pos = RandomCircle(center, radius, angle);
-            Instantiate(prefab, pos, Quaternion.identity);
+        for(int i = 0; i < 100; i ++){
+            for(int row = 1; row <= 36; i++){
+                Vector3 pos = RandomCircle(center, radius, row * 10);
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+            
         
         }
     }
 
     private float DetermineAngle(){
-        int num = MainManager.Instance.cornStart;
-        if(num > 500){
+        //int num = MainManager.Instance.cornStart;
+        return 360;
+        /*if(num > 500){
             return 360;
         }else if(num <= 500 && num > 250){
             return 270;
@@ -38,7 +43,7 @@ public class SpawnCropManager : MonoBehaviour
             return 180;
         }else{
             return 90;
-        }
+        }*/
     }
 
     private float DetermineRadius(){
@@ -46,8 +51,8 @@ public class SpawnCropManager : MonoBehaviour
     }
 
     Vector3 RandomCircle (Vector3 center, float radius, float angle){
-         float ang = Random.value * angle;
-         float nRadius = (Random.value * radius) + 5;
+         float ang = angle;//Random.value * angle;
+         float nRadius = (Random.value * radius) + 2;
          Vector3 pos;
          pos.x = center.x + nRadius * Mathf.Sin(ang * Mathf.Deg2Rad);
          pos.y = center.y;
