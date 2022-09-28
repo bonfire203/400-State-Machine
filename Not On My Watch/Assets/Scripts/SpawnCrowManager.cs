ad8ebf3ce436c3c02757d44fbafb078b3dc997f0
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2022-09-27 20:08:08 -0600

    crow spawns

--- Not On My Watch/Assets/Scripts/SpawnCrowManager.cs
+++ Not On My Watch/Assets/Scripts/SpawnCrowManager.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class SpawnCrowManager : MonoBehaviour
 {
     public GameObject prefab;
+    readonly float MAX_CORN_SPAWN = 216f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnCrow());   
     }
 
     IEnumerator SpawnCrow(){
-        
-        GameObject myCrow = Instantiate(prefab, pos, Quaternion.Euler( 0, Random.Range( 0, 360 ), 0 ));
-        
+        //determine spawn time to scale
+        float cornNum = MainManager.Instance.cornStart;
+            float factor = (MAX_CORN_SPAWN - cornNum) / 10;
+            Vector3 pos = new Vector3(Random.Range(25f, 50f), Random.Range(10f, 20f), Random.Range(25f, 50f));
+            GameObject myCrow = Instantiate(prefab, pos, Quaternion.identity);
+            Debug.Log(factor);
+            yield return new WaitForSeconds(5f);
+            StartCoroutine(SpawnCrow());  
     }
 }
